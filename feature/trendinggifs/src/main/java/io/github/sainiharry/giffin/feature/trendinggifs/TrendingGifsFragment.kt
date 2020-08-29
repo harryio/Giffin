48437215eeb23298a0fb2b832afac05562108fee
Harsimran Saini <harry.singh@mailbox.org>
2020-08-29 14:24:12 -0400

    Add swipe to refresh to trending gifs

--- feature/trendinggifs/src/main/java/io/github/sainiharry/giffin/feature/trendinggifs/TrendingGifsFragment.kt
+++ feature/trendinggifs/src/main/java/io/github/sainiharry/giffin/feature/trendinggifs/TrendingGifsFragment.kt
@@ -9,16 +9,20 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import io.github.sainiharry.giffin.feature.trendinggifs.databinding.FragmentTrendingGifsBinding
 import io.github.sainiharry.giffin.featurecommonfeature.GifAdapter
 import io.github.sainiharry.giffin.featurecommonfeature.LoadingAdapter
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.get
 
+@FlowPreview
 class TrendingGifsFragment : Fragment() {
 
     private val viewModel by viewModels<TrendingGifViewModel> {
@@ -66,9 +70,19 @@ class TrendingGifsFragment : Fragment() {
         binding.recyclerView.setHasFixedSize(true)
         binding.recyclerView.adapter = adapter
 
+        binding.refreshLayout.setOnRefreshListener {
+            pagingAdapter.refresh()
+        }
+
         lifecycleScope.launch {
             viewModel.gifListFlow.collectLatest { pagingData ->
                 pagingAdapter.submitData(pagingData)
+            }
+        }
+
+        lifecycleScope.launch {
+            pagingAdapter.loadStateFlow.debounce(100).collectLatest { loadStates ->
+                binding.refreshLayout.isRefreshing = loadStates.refresh is LoadState.Loading
             }
         }
     }
